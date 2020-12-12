610d4aa5675ffaeb4b06bfd8d68dedcd32012705
thomas.blc <thomas.blanc@webwag.com>
2020-12-12 19:16:37 +0100

    split theme config into file

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,3 +1,5 @@
+var theme_config = require("./theme"); 
+
 module.exports = {
   base: "",
   title: "Mamoru",
@@ -6,63 +8,7 @@ module.exports = {
     ['link', { rel: 'icon', href: "/favicon.ico" }],
     ['link', { rel: 'stylesheet', href: "https://use.typekit.net/rpa2arv.css" }]
   ],
-  themeConfig: {
-    navbar: true,
-    sidebar: {
-      '/portfolio/': [
-        ['/portfolio/', 'about'],
-        {
-          title: 'computer graphic',   // required
-          collapsable: false, // optional, defaults to true
-          sidebarDepth: 1,    // optional, defaults to 1
-          children: [
-            ['/portfolio/cg/three-graces', 'three-graces'],
-            ['/portfolio/cg/mercenary', 'mercenary'],
-            ['/portfolio/cg/ceson', 'ce son'],
-            ['/portfolio/cg/launchpad', 'launchpad'],
-            ['/portfolio/cg/poster-design', 'poster design'],
-          ]
-        },
-        {
-          title: 'game jams',   // required
-          collapsable: false, // optional, defaults to true
-          sidebarDepth: 1,    // optional, defaults to 1
-          children: [
-            ['https://macouta.itch.io/burning-desire', 'burning-desire'],
-            ['https://macouta.itch.io/bloom', 'bloom'],
-            ['https://macouta.itch.io/does-size-matter', 'does-size-matter'],
-            ['https://macouta.itch.io/the-prince-and-the-flower', 'prince-and-the-flower']
-          ]
-        },
-        {
-          title: 'developement',   // required
-          collapsable: false, // optional, defaults to true
-          sidebarDepth: 1,    // optional, defaults to 1
-          children: [
-            ['http://hakunamabh.cluster023.hosting.ovh.net/', 'old 3D UI prototype'],
-            ['https://github.com/Macouta?tab=repositories', 'github'],
-          ]
-        },
-      ],
-      '/': [
-        ['/', 'Home'],
-        ['/portfolio/', 'Portfolio'],
-      ],
-    },
-    search: false,
-
-    // if your docs are in a different repo from your main project:
-    docsRepo: 'Macouta/Mamoru',
-    // root of the doc:
-    docsDir: 'docs',
-    // branch of the doc:
-    docsBranch: 'master',
-    // default value is true. Allows to hide next page links on all pages
-    nextLinks: false,
-    // default value is true. Allows to hide prev page links on all pages
-    prevLinks: false,
-
-  },
+  themeConfig: theme_config,
   markdown: {
     anchor: { permalink: false, permalinkBefore: false, permalinkSymbol: '\u1557' }
   },
---
+++ docs/.vuepress/theme.js
@@ -0,0 +1,62 @@
+module.exports = {
+    navbar: true,
+    sidebar: {
+        '/portfolio/': [
+            ['/portfolio/', 'about'],
+            {
+                title: 'computer graphic',   // required
+                collapsable: false, // optional, defaults to true
+                sidebarDepth: 1,    // optional, defaults to 1
+                children: [
+                    ['/portfolio/cg/three-graces', 'three-graces'],
+                    ['/portfolio/cg/mercenary', 'mercenary'],
+                    ['/portfolio/cg/ceson', 'ce son'],
+                    ['/portfolio/cg/launchpad', 'launchpad'],
+                    ['/portfolio/cg/poster-design', 'poster design'],
+                ]
+            },
+            {
+                title: 'game jams',   // required
+                collapsable: false, // optional, defaults to true
+                sidebarDepth: 1,    // optional, defaults to 1
+                children: [
+                    ['https://macouta.itch.io/burning-desire', 'burning-desire'],
+                    ['https://macouta.itch.io/bloom', 'bloom'],
+                    ['https://macouta.itch.io/does-size-matter', 'does-size-matter'],
+                    ['https://macouta.itch.io/the-prince-and-the-flower', 'prince-and-the-flower']
+                ]
+            },
+            {
+                title: 'developement',   // required
+                collapsable: false, // optional, defaults to true
+                sidebarDepth: 1,    // optional, defaults to 1
+                children: [
+                    ['http://hakunamabh.cluster023.hosting.ovh.net/', 'old 3D UI prototype'],
+                    ['https://github.com/Macouta?tab=repositories', 'github'],
+                ]
+            },
+        ],
+        '/': [
+            ['/', 'Home'],
+            ['/lists/', 'References'],
+            ['/portfolio/', 'Portfolio'],
+        ],
+    },
+    search: false,
+    lastUpdated: 'updated',
+
+    repo: 'vuejs/vuepress',
+    // Customising the header label
+    // Defaults to "GitHub"/"GitLab"/"Bitbucket" depending on `themeConfig.repo`
+    repoLabel: 'Contribute!',
+
+    editLinks: true,
+    // custom text for edit link. Defaults to "Edit this page"
+    editLinkText: 'Help us improve this page!',
+    // if your docs are in a different repo from your main project:
+    // default value is true. Allows to hide next page links on all pages
+    nextLinks: false,
+    // default value is true. Allows to hide prev page links on all pages
+    prevLinks: false,
+
+}
